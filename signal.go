190b1b114c2fa43346d3c443262f9cf3e39363e5
Andrew Stuart <andrew.stuart2@gmail.com>
2022-03-04 11:46:58 -0700

    Remove sigCtx type as it now adds no value.

--- signal.go
+++ signal.go
@@ -8,12 +8,6 @@ import (
 	"syscall"
 )
 
-// sigCtx is a context that will be cancelled if certain signals are received.
-// Its Err will include details if this is the reason it was cancelled.
-type sigCtx struct {
-	context.Context
-}
-
 // ForSignals returns a context.Context that will be cancelled if the given
 // signals (or SIGTERM and SIGINT by default, if none are passed) are received
 // by the process.
@@ -25,13 +19,10 @@ func ForSignals(sigs ...os.Signal) context.Context {
 		sigs = []os.Signal{syscall.SIGTERM, syscall.SIGINT}
 	}
 
-	sc := &sigCtx{Context: ctx}
-
 	ch := make(chan os.Signal, 2)
 	signal.Notify(ch, sigs...)
 
 	go func() {
-
 		i := 0
 		for sig := range ch {
 			i++
@@ -42,5 +33,5 @@ func ForSignals(sigs ...os.Signal) context.Context {
 		}
 	}()
 
-	return sc
+	return ctx
 }
